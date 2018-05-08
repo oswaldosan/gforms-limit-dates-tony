c661237d794ff6f6c7c285d8f9dfff4857b2368e
Oswaldo Sanchez <mago.ads@gmail.com>
2018-05-08 09:17:50 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,33 @@
+/*  Gravity Forms - date limiter  */
+
+    var d = new Date();   /* load the date */
+    var dia = d.getDay()  /* load the specific day */  /* Monday = 1, Tuesday = 2, wednesday = 3, thursday = 4 etc... */
+    var hora = d.getHours();  /* load the hour */
+ 
+
+
+    if (dia == '5' || hora > 9) {     /* Condition for check the specific day or hour */
+   /* in this condition we check if its friday or if its 9am or more. */
+
+
+gform.addFilter( 'gform_datepicker_options_pre_init', function( optionsObj, formId, fieldId ) {
+    if ( formId == 44 && fieldId == 1 ) {   /* Our form ID and field ID */
+        optionsObj.minDate = +4;   /* DISABLE 4 DAYS FROM TODAY  */
+        optionsObj.onClose = function (dateText, inst) {
+             jQuery('#input_44_1').datepicker('option', 'minDate', dateText).datepicker('setDate', dateText);
+        };
+    }
+    return optionsObj;
+});
+  
+
+} else {  
+
+
+      /* DO stuff or not */
+
+	 	 
+
+}
+
+
